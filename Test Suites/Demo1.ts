069ff2bd47d40bf2ad9de906c888d6854aa9bd61
AugustoRoa <augusto.roa@pragma.com.co>
2023-04-11 11:30:50 -0500

    Adds a Test suit

---
+++ Test Suites/Demo1.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Demo1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e20f9b6f-15bb-4336-b560-7e2efd1a2001</testSuiteGuid>
+   <testCaseLink>
+      <guid>8fe8bfad-6028-49e0-ad1b-f41b87adb3d4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/LoginAppBad</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
